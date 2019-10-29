116f65507cfd4ab871526ae428cea8209b75ca09
comanezz <aymeric.lcharles@gmail.com>
2019-10-29 22:26:45 +0000

    code beautified

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,16 @@
-    function sendMail(contactForm) {
-        emailjs.send("gmail", "myform", {
-            "from_name": contactForm.user_name.value,
-            "from_email": contactForm.user_mail.value,
-            "message_request": contactForm.user_message.value
-        })
-            .then(
-                function (response) {
-                    console.log("SUCCESS", response);
-                },
-                function (error) {
-                    console.log("FAILED", error);
-                }
-            );
-        return false;  // To block from loading a new page
-    };
+function sendMail(contactForm) {
+    emailjs.send("gmail", "myform", {
+        "from_name": contactForm.user_name.value,
+        "from_email": contactForm.user_mail.value,
+        "message_request": contactForm.user_message.value
+    })
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
+};
