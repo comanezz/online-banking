b60513d3de594e2101778a9eb5863fc10e94309a
comanezz <aymeric.lcharles@gmail.com>
2019-10-29 22:26:18 +0000

    comments removed

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -60,39 +60,4 @@ $(document).ready(function() {
             alert('valid form submitted');
         }
     });
-    
-    // Section Part
-    // Animation effect
-    // Found how to animate elements when scrolling through this webpage. https://www.sitepoint.com/scroll-based-animations-jquery-css3/
-    // var $animation_elements = $('.animation-element');
-    
-    // Handling Resizing
-    // $window.on('scroll resize', check_if_in_view);
-    
-    /* In addition, we also use the jQuery trigger method to trigger a scroll event as soon as the DOM is ready. 
-    We do this so that if any of the elements which should be animated are within the viewport,
-    they will be detected as in view and the animation applied as if we had scrolled. */
-    // $window.trigger('scroll');
-    
-    
-    // function check_if_in_view() {
-    //     var window_height = $window.height();
-    //     var window_top_position = $window.scrollTop();
-    //     var window_bottom_position = (window_top_position + window_height);
-        
-    //     // function can be used to iterate over any collection
-    //     $.each($animation_elements, function() {
-    //        var $element = $(this);
-    //        var element_height = $element.outerHeight();
-    //        var element_top_position = $element.offset().top;
-    //        var element_bottom_position = (element_top_position + element_height);
-           
-    //        //check to see if this current container is within viewport
-    //        if ((element_top_position <= window_bottom_position) && (element_bottom_position >= window_top_position)) {
-    //            $element.addClass('in-view');
-    //        } else {
-    //            $element.removeClass('in-view');
-    //        }
-    //     });
-    // };
 });
