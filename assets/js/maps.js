e46484fdd47a07e3246deff1e0404cc1ba776461
comanezz <aymeric.lcharles@gmail.com>
2019-11-10 21:20:46 +0000

    code beautified

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,20 +1,20 @@
-        var map;
-        function initMap() {
-          var marseilleStade = {lat: 43.2698, lng: 5.3959};
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: marseilleStade,
-            zoom: 16
-          });
-          
-          var velodrome = {lat: 43.2698, lng: 5.3959};
-          var marker = new google.maps.Marker({position:velodrome, map: map});
-          
-          var aix = {lat:43.5263, lng:5.4454};
-          map = new google.maps.Map(document.getElementById('map2'), {
-            center: aix,
-            zoom: 16
-          });
-          
-          var fontaineAix = {lat:43.5263, lng:5.4454}
-          var marker = new google.maps.Marker({position: fontaineAix, map: map});
-        }
+var map;
+function initMap() {
+  var marseilleStade = { lat: 43.2698, lng: 5.3959 };
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: marseilleStade,
+    zoom: 16
+  });
+
+  var velodrome = { lat: 43.2698, lng: 5.3959 };
+  var marker = new google.maps.Marker({ position: velodrome, map: map });
+
+  var aix = { lat: 43.5263, lng: 5.4454 };
+  map = new google.maps.Map(document.getElementById('map2'), {
+    center: aix,
+    zoom: 16
+  });
+
+  var fontaineAix = { lat: 43.5263, lng: 5.4454 }
+  var marker = new google.maps.Marker({ position: fontaineAix, map: map });
+}
