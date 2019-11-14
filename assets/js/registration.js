0df3b555de2492dbe52908023d7cb677490cf301
Aymeric Louis-Charles <alouischarles@dubmmfzf09nlywq.hubspot.local>
2019-11-14 18:25:35 +0000

    Regex code shorten, still working

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -22,10 +22,6 @@ $(document).ready(function() {
 
     function isEmail(email) {
         var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        if (!regex.test(email)) {
-            return false;
-        } else {
-            return true;
-        }
+        return regex.test(email)
     }
 });
