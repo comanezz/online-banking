45ea7e9e8180622c5dc075e03537878d47ebb444
Aymeric Louis-Charles <alouischarles@dubmmfzf09nlywq.hubspot.local>
2019-11-14 18:32:08 +0000

    comment added, variable renamed

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,22 +1,24 @@
-var map;
 function initMap() {
-  var marseilleStade = { lat: 43.2698, lng: 5.3959 };
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: marseilleStade,
+  
+  // Headquarter
+  var marseilleStadium = { lat: 43.2698, lng: 5.3959 };
+  marseille = new google.maps.Map(document.getElementById('map'), {
+    center: marseilleStadium,
     zoom: 16,
     disableDefaultUI: true
   });
 
   var velodrome = { lat: 43.2698, lng: 5.3959 };
-  var marker = new google.maps.Marker({ position: velodrome, map: map });
+  var marker = new google.maps.Marker({ position: velodrome, map: marseille });
 
+  // Second Office
   var aix = { lat: 43.5263, lng: 5.4454 };
-  map2 = new google.maps.Map(document.getElementById('map2'), {
+  aixEnProvence = new google.maps.Map(document.getElementById('map2'), {
     center: aix,
     zoom: 16,
     disableDefaultUI: true
   });
 
   var fontaineAix = { lat: 43.5263, lng: 5.4454 }
-  var marker = new google.maps.Marker({ position: fontaineAix, map: map2 });
+  var marker = new google.maps.Marker({ position: fontaineAix, map: aixEnProvence });
 }
