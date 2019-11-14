ee66dfd08f448c2af248b5d4b8f6ffe32c90443e
Aymeric Louis-Charles <alouischarles@dubmmfzf09nlywq.hubspot.local>
2019-11-14 18:20:38 +0000

    jasmine deleted. No need

--- assets/jasmine-testing/spec/jquerySpec.js
+++
@@ -1,7 +0,0 @@
-describe("jQuery comment Plugin", function() {
-    describe("jQuery tests", function() {
-        it('Should find jQuery', function() {
-            expect($).toBeNull();
-        });
-    });
-});
